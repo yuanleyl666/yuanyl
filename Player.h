09c36194020fad89bd0ef66442e3aad920a0b44b
machineNotWork <85060785+machineNotWork@users.noreply.github.com>
2021-06-19 14:12:05 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,54 +1,16 @@
 #pragma once
 #include "ui/CocosGUI.h"
 #include "ChessBase.h"
-#include"Menu//myMenu.h"
+#include"myMenu.h"
 #include"ChessWithSprite.h"
-#include"Shop.h"
 #include"PlayerBoard.h"
+#include "Shop.h"
 using namespace cocos2d;
-class ChessWithSprite;
-class shop;
+class Shop;
 class PlayerBoard;
+
 class Player
 {
-public:
-    const int getPlayerRank()const;
-    const int getExp()const;
-    const int getLevel()const;
-    const int getGold()const;
-    const int getHp()const;
-    Sprite* getSprite()const
-    {
-        return this->PlayerSprite;
-    }
-    ChessWithSprite* getReserve(const int i);
-
-    bool setReserve(const int i, ChessWithSprite*);
-    bool setPlayerRank(const int playerRank);
-    bool setExp(const int Exp);
-    bool setLevel(const int Level);
-    bool setGold(const int Gold);
-    bool setHp(const int Hp);
-    bool setSprite(Sprite* sp)
-    {
-        this->PlayerSprite = sp;
-    }
-    Shop* getShop();
-    void setShop(Shop*);
-    void openShop()
-    {
-        Shop* toOpen = new Shop(this);
-
-        this->PlayerSprite->addChild(toOpen->getSprite(), 0, 1);
-    }
-    PlayerBoard* getPlayerBoard()
-    {
-        return this->board;
-    }
-    void setPlayerBoard(PlayerBoard* toSet)
-    {
-        this->board = toSet;
-    }
 private:
     int playerRank;
     int exp;
@@ -60,5 +22,29 @@ private:
     Sprite* PlayerSprite;
     PlayerBoard* board;
     Shop* ShopOpened;
+public:
+    const int getPlayerRank()const;
+    const int getExp()const;
+    const int getLevel()const;
+    const int getGold()const;
+    const int getHp()const;
+    Sprite* getSprite()const;
+    ChessWithSprite* getReserve(const int i);
+    Shop* getShop();
+    PlayerBoard* getPlayerBoard();
+
+    bool setReserve(const int i, ChessWithSprite*);
+    bool setPlayerRank(const int playerRank);
+    bool setExp(const int Exp);
+    bool setLevel(const int Level);
+    bool setGold(const int Gold);
+    bool setHp(const int Hp);
+    bool setSprite(Sprite* sp);
+    void setPlayerBoard(PlayerBoard* toSet);
+    void setShop(Shop* shop);
+
+    void openShop();
+
+
 
 };
