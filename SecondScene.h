12e81726f86e38efd32eff4dfdbe6f56b95dab0c
Yumo Xia <85444182+ConstantineXYM@users.noreply.github.com>
2021-06-19 21:37:03 +0800

    Update SecondScene.h

--- SecondScene.h
+++ SecondScene.h
@@ -1,8 +1,10 @@
 #pragma once
 #include"cocos2d.h"
+#include "Player.h"
+#include "PlayerBoard.h"
 
 using namespace cocos2d;
-
+class PlayerBoard; class Player;
 class MySecondScene :public Layer
 {
 public:
@@ -14,6 +16,22 @@ public:
 
 	void EnterFirstScene(Ref* pSender);
 
-
 	void menuCloseCallback(cocos2d::Ref* pSender);
+
+	//myadd
+	void moveChess();
+	Player* getPlayer();
+	void setPlayer(Player*);
+	PlayerBoard* getBoard();
+	void setBoard(PlayerBoard*);
+	//MySecondScene* getScene();
+	//void setScene(MySecondScene*);
+private:
+	Sprite* targetA = nullptr;
+	Player* player = nullptr;
+	PlayerBoard* board = nullptr;
+	//MySecondScene* scene;
+private:
+	Point pos, posA, pos_org;
+	int x, y, x_org, y_org;
 };
