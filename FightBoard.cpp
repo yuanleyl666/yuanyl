eb5b2137b6759bd672b125a6037a0658a5da9539
yuanleyl666 <83457351+yuanleyl666@users.noreply.github.com>
2021-06-19 19:40:01 +0800

    更新FightBoard（可在特定坐标处攻击）

--- FightBoard.cpp
+++ FightBoard.cpp
@@ -4,57 +4,65 @@ using namespace cocos2d;
 void FightBoard::attack(const int xInit, const int yInit, const int xAim, const int yAim)
 {
     chess[yInit][xInit].attack(&chess[xAim][yAim]);
+
+
+    //需要添加攻击精灵的位置
+
+
     //攻击动画（上侧）
     auto spriteA = Sprite::create("/res/atk.png");
     spriteA->setScale(0.1);
-    spriteA->setPosition(100, 100);
+    spriteA->setPosition(xInit*16.0f, yInit * 16.0f);
     spriteA->setRotation(-70.0f);
     //this->getSprite()->getParent()代表场景的this指针
     this->chess[yInit][xInit].getSprite()->getParent()->addChild(spriteA);
-    auto rotateToA = RotateBy::create(0.6f, -30.0f);
-    auto seqA = Sequence::create(rotateToA, rotateToA->reverse(), nullptr);
+    auto rotateToA = RotateBy::create(0.475f, -30.0f);
+    auto fadeOut = FadeOut::create(0.05f);
+    auto seqA = Sequence::create(rotateToA, rotateToA->reverse(),fadeOut, nullptr);
     //攻击动画（下侧）
     auto spriteB = Sprite::create("/res/atk.png");
     spriteB->setScale(0.1);
-    spriteB->setPosition(100, 100);
+    spriteB->setPosition(xInit * 16.0f, yInit * 16.0f);
     spriteB->setRotation(-15.0f);
     //this->getSprite()->getParent()代表场景的this指针
     this->chess[yInit][xInit].getSprite()->getParent()->addChild(spriteB);
-    auto rotateToB = RotateBy::create(0.6f, 30.0f);
-    auto seqB = Sequence::create(rotateToB, rotateToB->reverse(), nullptr);
+    auto rotateToB = RotateBy::create(0.475f, 30.0f);
+    auto seqB = Sequence::create(rotateToB, rotateToB->reverse(),fadeOut, nullptr);
     //远程攻击
-    auto moveTo = MoveTo::create(4, Vec2(300, 300));
+    auto moveTo = MoveTo::create(0.95f, Vec2(xAim*16.0f, yAim * 16.0f));
     auto emitter = ParticleFire::create();//火焰
-    emitter->setScale(0.15);
-    emitter->setPosition(50, 50);
-    this->chess[yInit][xInit].getSprite()->getParent()->addChild(spriteB);
+    emitter->setScale(0.15f);
+    emitter->setPosition(xInit*16.0f, yInit * 16.0f);
+    this->chess[yInit][xInit].getSprite()->getParent()->addChild(emitter);
+    auto seqC = Sequence::create(moveTo, fadeOut, nullptr);
     // 技能特效
     auto emitter6 = ParticleExplosion::create();//爆炸
     emitter6->setScale(0.5);
-    emitter6->setPosition(50, 50);
-    this->chess[yInit][xInit].getSprite()->getParent()->addChild(spriteB);
+    emitter6->setPosition(xInit * 16.0f, yInit * 16.0f);
+    this->chess[yInit][xInit].getSprite()->getParent()->addChild(emitter6);
+    auto seqD = Sequence::create(moveTo, fadeOut, nullptr);
     //通过chesstype判断攻击类型
     switch (this->chess[yInit][xInit].ChessWithSprite::getChessType())
     {
 
-    case 0:
-        break;
-    case 1:
-        //攻击动画（上侧）
-        spriteA->runAction(seqA);
-        break;
-    case 2:
-        //攻击动画（下侧）
-        spriteB->runAction(seqB);
-        break;
-    case 3:
-        //远程攻击
-        emitter->runAction(moveTo);
-        break;
-    case 4:
-        // 技能特效
-        emitter6->runAction(moveTo);
-        break;
+        case 0:
+            break;
+        case 1:
+            //攻击动画（上侧）
+            spriteA->runAction(seqA);
+            break;
+        case 2:
+            //攻击动画（下侧）
+            spriteB->runAction(seqB);
+            break;
+        case 3:
+            //远程攻击
+            emitter->runAction(seqC);
+            break;
+        case 4:
+            // 技能特效
+            emitter6->runAction(seqD);
+            break;
     }
     if (chess[xAim][yAim].getHP() < 1e-3)
     {
@@ -137,121 +145,4 @@ const positionOnMap FightBoard::findPossibleAttackAim(const int xInBoard, const 
         }
     }
     return toReturn;
-}
-void FightBoard::moveChess(const int xInit, const int yInit, const int xAim, const int yAim)
-{
-    if (this->chess[yInit][xInit].getChessType() == ChessWithSprite::ChessType::noChess)
-        return;
-    this->chess[yInit][xInit].changeChessToOtherChess(&this->chess[yAim][xAim]);
-    double distance = 16;
-    auto operateSprite = this->chess[yAim][xAim].getSprite()->getChildByTag(0);
-    operateSprite->setPosition(operateSprite->getPositionX() - (xAim - xInit), operateSprite->getPositionY() - (yAim - yInit));
-    // Move a sprite to a specific location over 2 seconds.
-    auto moveTo = MoveTo::create(2, Vec2(xAim - xInit, yAim - yInit));
-    operateSprite->runAction(moveTo);
-}
-void FightBoard::act(bool& player_1_lose, bool& player_2_lose)
-{
-    bool isActed[8][8] = { 0 };
-    for (int i = 0; i < 8; i++)
-        for (int k = 0; k < 8; k++)
-        {
-            if (chess[i][k].getChessType() && !isActed[i][k])
-            {
-
-                if (chess[i][k].getPlayer() == player1->getPlayerRank())
-                    player_1_lose = 0;
-                else if (chess[i][k].getPlayer() == player2->getPlayerRank())
-                    player_2_lose = 0;
-
-                positionOnMap atkPosition = this->findPossibleAttackAim(k, i);
-                positionOnMap movePosition = this->findNextMove(k, i);
-                if ((movePosition == positionOnMap(k, i)) == 0)
-                {
-                    moveChess(k, i, movePosition.x, movePosition.y);
-                    log("MOVE");
-                    log("%d,%d", movePosition.x, movePosition.y);
-                    isActed[movePosition.y][movePosition.x] = 1;
-                }
-                else
-                {
-                    chess[i][k].attack(&chess[atkPosition.y][atkPosition.x]);
-                    isActed[atkPosition.y][atkPosition.x] = 1;
-                    CCLOG("ATK");
-                }
-            }
-        }
-
-}
-//
-void FightBoard::fight()
-{
-    if (getFightWinner() == this->player1->getPlayerRank())
-    {
-        player1->setHp(player1->getHp() - 1);
-    }
-    if (getFightWinner() == this->player2->getPlayerRank())
-    {
-        player2->setHp(player2->getHp() - 1);
-    }
-}
-
-bool FightBoard::init(Player* p1, Player* p2)
-{
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-    this->boardScene = Scene::create();
-
-    auto MapSprite = Sprite::create("HelloWorld.png");
-    MapSprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-    boardScene->addChild(MapSprite);
-
-    for (int i = 0; i < 8; i++)
-        for (int k = 0; k < 8; k++)
-        {
-            MapSprite->addChild(chess[i][k].getSprite());
-            auto chessSprite = chess[i][k].getSprite();
-            chessSprite->setScale(0.12, 0.12);
-            auto move = MoveTo::create(0, Vec2(20 + k * 25, 50 + i * 25));
-            chessSprite->runAction(move);
-        }
-
-    this->player1 = p1;
-    this->player2 = p2;
-    for (int i = 0; i < 8; i++)
-    {
-        for (int k = 0; k < 8; k++)
-        {
-            if (i < 4)
-            {
-                this->chess[i][k] = *(p1->getPlayerBoard()->getChess(i, k));
-
-            }
-            else
-            {
-                this->chess[i][k] = *(p2->getPlayerBoard()->getChess(7 - i, 7 - k));
-
-            }
-        }
-    }
-    return 1;
-}
-Scene* FightBoard::getBoardScene()
-{
-    return this->boardScene;
-}
-void FightBoard::setBoardScene(Scene* toSet)
-{
-    this->boardScene = toSet;
-}
-void FightBoard::Winner(bool player1_lose)
-{
-    if (player1_lose == 1)
-        this->fightWinner = player2->getPlayerRank();
-    else
-        this->fightWinner = player1->getPlayerRank();
-}
-int FightBoard::getFightWinner()
-{
-    return fightWinner;
 }
