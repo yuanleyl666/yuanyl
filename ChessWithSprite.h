63412a8b33eda2e5e5c143fbcb6328495097e8f1
machineNotWork <85060785+machineNotWork@users.noreply.github.com>
2021-06-19 14:08:19 +0800

    Create ChessWithSprite.h

---
+++ ChessWithSprite.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <cmath>
+#include "cocos2d.h"
+#include"ChessBase.h"
+using namespace cocos2d;
+
+
+//在基类基础上放置了精灵
+class ChessWithSprite :public ChessBase
+{
+public:
+    ChessWithSprite()
+    {
+        //这里应该把精灵换全透明
+        this->sprite = Sprite::create("HelloWorld.png");
+       this-> sprite->setOpacity(0);
+    }
+    //需要更改数据
+    enum ChessType { noChess, Axe, CG, BH, DR, CK, MR, QoP, Jugg, TK, DS, PA, TW, GG, GS, DK, TA, DRK, GT, TH, COCO };
+
+    //复制构造，在复制构造过程中，精灵也会复制并绑定到新对象，会初始化棋子的数据
+    ChessWithSprite(const ChessWithSprite& chess);
+
+    //赋值，只赋值不初始化，用于战斗等，精灵也会复制并绑定到新对象
+    ChessWithSprite& operator=(const ChessWithSprite& chess);
+
+    //根据棋子的type进行构造
+    ChessWithSprite(const ChessWithSprite::ChessType& type) : ChessWithSprite()
+    {
+        this->chessType = type;
+        this->init();
+    }
+    
+    //只设定棋子绑定的原精灵（透明的精灵）（基本弃用）
+    bool setSprite(Sprite*);
+
+    //只得到棋子绑定的原精灵（透明的精灵）
+    Sprite* getSprite()const;
+
+    //攻击
+    
+    //需要修改攻击动画（未完成）
+    bool attack(ChessWithSprite* targetChess);
+
+    void setChessType(const ChessType& type);
+
+    ChessType getChessType()const;
+    
+    //根据棋子的type进行初始化并绑定对应type精灵
+    void init();
+    void initInShop();
+    //把参数的棋子换成调用的棋子并且把调用函数的棋子变为nochess
+    bool changeChessToOtherChess(ChessWithSprite* chess)
+    {
+        if (this->chessType == noChess || chess->chessType != noChess)
+            return 0;
+
+        *chess = *this;
+
+        this->sprite->removeAllChildren();
+        this->chessType = noChess;
+        this->init();
+    }
+private:
+    ChessType chessType = noChess;
+
+    Sprite* sprite;
+};
