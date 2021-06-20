8770a37e2ea57300008a3d6e2ceb352d975c46ba
Yumo Xia <85444182+ConstantineXYM@users.noreply.github.com>
2021-06-20 10:19:37 +0800

    Update Shop.h

--- Shop.h
+++ Shop.h
@@ -12,24 +12,25 @@ private:
     Player* whoOpenThis;
     Sprite* shopSprite;
     ChessWithSprite ChessLibrary[5];
-
     Shop(const Shop&) = delete;
     Shop() = delete;
-    Shop& operator=(const Shop&) = delete;
-    friend Player;
-    Shop(Player* player);
-    void closeShop();
+    Shop& operator =(const Shop&) = delete;
 public:
     //根据player的等级等初始化shop
-    friend void shopUpDate(Shop* shop, int pool[21]);
-  
+    ~Shop()
+    {
+        CC_SAFE_DELETE(shopSprite);
+    }
+    friend void shopUpDate(Shop* shop);
+    Shop(Player* player);
+
     Sprite* getSprite();
 
     void setSprite(Sprite*);
     ///买棋子
     bool buyChess(const int i);
     //ui关闭商店,同时断开商店和player的指针
-  
+    void closeShop();
 
     Player* getPlayer();
 
