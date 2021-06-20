8368b5c46851b1cbbff984b6e83cbd12b5e5e984
Yumo Xia <85444182+ConstantineXYM@users.noreply.github.com>
2021-06-20 10:17:04 +0800

    Update PlayerBoard.h

--- PlayerBoard.h
+++ PlayerBoard.h
@@ -4,30 +4,44 @@
 #include"ChessBoard.h"
 #include "myMenu.h"
 #include "Player.h"
+#include "SecondScene.h"
 using namespace cocos2d;
 class Player;
-class myMenu;
+class MySecondScene;
 class PlayerBoard :public ChessBoard
 {
 public:
-    Scene* getBoardScene();
-    void setBoardScene(Scene*);
+    MySecondScene* getBoardScene();
+    void setBoardScene(MySecondScene*);
+    //myadd
+    Sprite* getMap();
+    void setMap(Sprite*);
+
+    void init()
+    {
+        if (this->MapSprite == nullptr )
+            return;
+        Point ppos;
+        for (int i = 0; i < 8; i++)
+            for (int k = 0; k < 8; k++)
+            {
+                if (chess[i][k].getSprite() == nullptr)
+                    continue;
+                auto chessSprite = chess[i][k].getSprite();
+                ppos.x = 177 + 16 * i;
+                ppos.y = 100 + 16 * k;
+                chessSprite->setPosition(ppos);
+                MapSprite->addChild(chessSprite,3);
+            }
+    }
      Player* getPlayer();
     void setPlayer(Player*);
-    void setMap(Sprite* map)
-    {
-        this->MapSprite = map;
-    }
-    Sprite* getMap()
-    {
-        return this->MapSprite;
-    }
     virtual bool setChess(const int& row, const int& column, ChessWithSprite* chess);
     PlayerBoard();
 
 private:
     Sprite* MapSprite = nullptr;
-   Scene* boardScene = nullptr;
+    MySecondScene* boardScene = nullptr;
     Player* player = nullptr;
 
 };
